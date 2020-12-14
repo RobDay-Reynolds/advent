08576a78bfc0488fb4a864dbfd5394105eb88a84
Rob Day-Reynolds <rdayreynolds@newrelic.com>
2020-12-13 23:38:55 -0800

    Include both answers in Day 1

--- day1/sum_and_multiply.rb
+++ day1/sum_and_multiply.rb
@@ -2,23 +2,41 @@
 
 input = File.readlines('input.txt').map(&:chomp)
 
-input.each do |number|
-  number = number.to_i
-  if number < 2020
-    input.each do |second_number|
-      second_number = second_number.to_i
-      sum = number + second_number
-      if sum < 2020
-        input.each do |third_number|
-          third_number = third_number.to_i
+def multiply_two_numbers(input)
+  input.each do |number|
+    number = number.to_i
+    if number < 2020
+      input.each do |second_number|
+        second_number = second_number.to_i
+  
+        if number + second_number == 2020
+          return number * second_number
+        end
+      end
+    end
+  end
+end
 
-          if sum + third_number == 2020
-            puts number * second_number * third_number
-
-            return
+def multiply_three_numbers(input)
+  input.each do |number|
+    number = number.to_i
+    if number < 2020
+      input.each do |second_number|
+        second_number = second_number.to_i
+        sum = number + second_number
+        if sum < 2020
+          input.each do |third_number|
+            third_number = third_number.to_i
+  
+            if sum + third_number == 2020
+              return number * second_number * third_number
+            end
           end
         end
       end
     end
   end
 end
+
+puts "With two numbers: #{multiply_two_numbers(input)}"
+puts "With three numbers: #{multiply_three_numbers(input)}"
